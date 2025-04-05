b8ddf840a94b85a3b382da5f4e469433ad3ae76a
queniy <yingzi110970@gmail.com>
2025-04-05 23:52:40 +0200

    Update re.js

--- re.js
+++ re.js
@@ -1,13 +1,14 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+if (window.location.href == "Pages.html") {document.addEventListener("DOMContentLoaded", ()=>{
         if (localStorage.getItem("a")) {alert("You are not logged In");window.location.href = "main.html";};})
-const params = new URLSearchParams(window.location.search);
-                    const value = params.get("a");
-                    if (!value) {
-                        let username = localStorage.getItem("rusername");
-                        document.getElementById("Username").innerHTML = username;
-                    } else {
-                        document.getElementById("Username").innerHTML = "anonymous";
-                    }
+}
+if (window.location.href == "Pages.html" || window.location.href == "Pages.html") {
+const value = localStorage.getItem("a");
+if (!value) {
+        let username = localStorage.getItem("rusername");
+         document.getElementById("Username").innerHTML = username;
+} else {
+        document.getElementById("Username").innerHTML = "anonymous";
+}}
 if (window.location.href == "index.html") {
         localStorage.deleteItem("a");
 }
