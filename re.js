35a7cb9a0e68dd47a739f33e3206c0a21a9b4448
queniy <yingzi110970@gmail.com>
2025-04-05 23:44:09 +0200

    Update re.js

--- re.js
+++ re.js
@@ -1,2 +1,10 @@
 document.addEventListener("DOMContentLoaded", ()=>{
         if (localStorage.getItem("a")) {alert("You are not logged In");window.location.href = "main.html";};})
+const params = new URLSearchParams(window.location.search);
+                    const value = params.get("a");
+                    if (!value) {
+                        let username = localStorage.getItem("rusername");
+                        document.getElementById("Username").innerHTML = username;
+                    } else {
+                        document.getElementById("Username").innerHTML = "anonymous";
+                    }
