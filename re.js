ba7136478f26b845e657fd5d1bdfe81bb750588e
queniy <yingzi110970@gmail.com>
2025-04-06 00:01:06 +0200

    Update re.js

--- re.js
+++ re.js
@@ -1,7 +1,7 @@
 if (window.location.href == "Pages.html") {document.addEventListener("DOMContentLoaded", ()=>{
         if (localStorage.getItem("a")) {alert("You are not logged In");window.location.href = "main.html";};})
 }
-if (window.location.href == "Pages.html" || window.location.href == "Pages.html") {
+if (window.location.href == "main.html" || window.location.href == "Pages.html") {
 const value = localStorage.getItem("a");
 if (!value) {
         let username = localStorage.getItem("rusername");
